9644e49f3da9528f5676c7467b3eba0ee47cde55
cuffk_000 <cuffk_000@10.110.57.253>
2019-04-11 10:59:13 +0200

    Testing difficulty as function of time for levelGen.java

--- src/levelGenerator/LevelGen.java
+++ src/levelGenerator/LevelGen.java
@@ -1,17 +1,19 @@
 package levelGenerator;
 
+import org.jsfml.system.Clock;
+
 public class LevelGen{
 	private static float elapsed_time;
 	public static void update(float dt){
 		elapsed_time += dt;
 	}
 	public static float difficulty(){
-		float diff = 10*elapsed_time + 100;
-		//Linear increase in difficulty, 10 is gradient and 100 is base difficulty.
+		float diff = 10*elapsed_time;
+		//Linear increase in difficulty, 0.05f is the gradient and 1 is base difficulty.
 		return diff;
 	}
 	public static float health(float diff, EnemySpecs a) {
-		float health = 10*elapsed_time + a.getStartHealth();
+		float health = 0.05f*elapsed_time + a.getStartHealth();
 		return health;
 	}
 	public static float damage(float diff, EnemySpecs b){
@@ -20,6 +22,19 @@ public class LevelGen{
 	}
 	public static void reset(){
 		elapsed_time = 0.0f;
+	}
+	
+	public static void main(String[] args) {
+		float et = 0.0f;
+		Clock clk = new Clock();
+		while(et < 10){
+			float dt = 10*clk.restart().asSeconds(); /* The 10 is the scale at which the time difference is multiplied */
+			et += dt;
+			//Testing
+			float diff = difficulty();
+			System.out.println(diff);
+			update(dt);
+		}
 	}
 		
 }
