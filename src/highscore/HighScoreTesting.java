a8ca709db47e6636f4e95e917ba96db6cadf0938
test01 <test01@10.109.7.120>
2019-04-12 18:03:39 +0200

    Ellooo, hier is die high score, sowel as die high score test funksie. Lemme know as dit werk

--- src/highscore/HighScoreTesting.java
+++ src/highscore/HighScoreTesting.java
@@ -1,10 +1,41 @@
 package highscore;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.*;
+
+
+/**
+ * @author Rita
+ * 
+ * 
+ *
+ */
 public class HighScoreTesting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		File oldhsfilex = new File("C:/Users/test01/git/Invaders-project/highscoredata/savedhs.txt");
+		try
+		{
+			int currentscore = 0; 
+			Scanner scantextx = new Scanner(oldhsfilex);
+			int hsoldx = 0;
+			hsoldx = scantextx.nextInt();
+			System.out.print("High score was " + hsoldx + "\n");
+			LoadHighscore.saveScoreIfGreater(currentscore);
+			
+			File newhsfilex = new File("C:/Users/test01/git/Invaders-project/highscoredata/savedhs.txt");
+			Scanner scantextnew = new Scanner(newhsfilex);
+			int hsnewx = scantextnew.nextInt();
+			System.out.print("High score is " + hsnewx + "\n");
+			scantextx.close();
+			scantextnew.close();
+		} catch(FileNotFoundException e) {
+			System.out.print("Error compiling");
+			
+		}
+		
 	}
-
 }
--- src/highscore/LoadHighscore.java
+++ src/highscore/LoadHighscore.java
@@ -1,5 +1,48 @@
 package highscore;
 
-public class LoadHighscore {
+import java.io.File;
+import java.util.Scanner;
+import java.io.*;
 
+
+//load single highscore from txt
+//allow to save highscore if the input paramater is greater than current highscore saveScoreIfGreater(int hs)
+//get highscore from file
+
+public class LoadHighscore {
+	public static void saveScoreIfGreater (int hs)// throws IOException
+	{
+		try
+		{
+			File oldhsfile = new File("C:/Users/test01/git/Invaders-project/highscoredata/savedhs.txt");
+			Scanner scantext = new Scanner(oldhsfile);
+			int hsold = 0;
+			hsold = scantext.nextInt();
+			if (hs>hsold)
+			{
+				oldhsfile.delete();
+				File newhssave = new File("C:/Users/test01/git/Invaders-project/highscoredata/savedhs.txt");
+				
+				try
+				{
+					FileWriter newhs = new FileWriter(newhssave, false);
+					newhs.write(hs + "");
+					newhs.close();
+					
+				} catch (IOException e){
+					
+				}
+			}
+			else
+			{
+				scantext.close();
+				return ;
+			}	
+			
+			scantext.close();
+		} catch(FileNotFoundException e) {
+			
+		}
+
+}
 }
